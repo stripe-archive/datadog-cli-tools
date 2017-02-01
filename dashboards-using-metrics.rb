e88dd701ad03753c57c3f6c31a6d5c97311b0a54
Kris Reeves <krisreeves@stripe.com>
2017-01-31 18:14:53 -0800

    Friendlier output with optional verbose mode for revealing which graphs on large dashboards are the culprit. Support for toggling regular expression escaping so you can write either a regex or a string to match. More specific matching against only the parts of the description that matter.

--- dashboards-using-metrics.rb
+++ dashboards-using-metrics.rb
@@ -3,27 +3,124 @@
 require_relative 'command'
 
 class DashboardsUsingMetrics < Command
+  def parse_args(options, parser)
+    options[:is_regex] = false
 
-  def run()
-    metric = ARGV[0] || raise("You must specify a metric name!")
+    parser.on('-r', '--[no-]regex', 'Input is a regular expression (don\'t quote)') do |v|
+      options[:is_regex] = v
+    end
+  end
 
-    @logger.info("Looking for dashboards using metric: '#{metric}'")
+  def metric_regex(metric)
+    if @options[:is_regex]
+      /#{metric}/
+    else
+      /#{Regexp.quote(metric)}/
+    end
+  end
+
+  def find_dashboard_graphs(definition, regex)
+    definition.fetch('dash', {}).fetch('graphs', []).reduce([]) do |acc, graph|
+      queries = graph.fetch('definition', {}).fetch('requests', []).reduce([]) do |acc2, request|
+        next acc2 unless request.key?('q') || !request['q'].respond_to?(:to_str)
+        next acc2 unless request['q'] =~ regex
+
+        acc2 + [request['q']]
+      end
+
+      next acc unless queries.length > 0
+
+      acc + [{
+        title: graph.fetch('title', 'Unknown'),
+        queries: queries
+      }]
+    end
+  end
+
+  def find_screenboard_graphs(definition, regex)
+    definition.fetch('widgets', []).reduce([]) do |acc, widget|
+      queries = widget.fetch('tile_def', {}).fetch('requests', []).reduce([]) do |acc2, request|
+        next acc2 unless request.key?('q') || !request['q'].respond_to?(:to_str)
+        next acc2 unless request['q'] =~ regex
+
+        acc2 + [request['q']]
+      end
+
+      next acc unless queries.length > 0
+
+      acc + [{
+        title: widget.fetch('title_text', 'Unknown'),
+        queries: queries
+      }]
+    end
+  end
+
+  def print_dashboard_matches(metric)
+    regex = metric_regex(metric)
+    @logger.info("Looking for dashboards using metric: #{regex.inspect}")
     _, dashes = @dog_client.get_dashboards()
     dashes['dashes'].each do |dash|
-      definition = @dog_client.get_dashboard(dash['id']).to_json
-      if definition =~ /#{Regexp.quote(metric)}/
-        puts(dash['id'])
-      end
-    end
+      status_code, definition = @dog_client.get_dashboard(dash['id'])
 
-    @logger.info("Looking for screenboards using metric: '#{metric}'")
-    _, screens = @dog_client.get_all_screenboards()
-    screens['screenboards'].each do |screen|
-      definition = @dog_client.get_screenboard(screen['id']).to_json
-      if definition =~ /#{Regexp.quote(metric)}/
-        puts(screen['id'])
+      if status_code != '200'
+        @logger.warn("Got status code #{status_code} querying dashboard #{dash['id']}")
+        next
+      end
+
+      found = find_dashboard_graphs(definition, regex)
+
+      if found.length > 0
+        url = template('dashboard', id: dash['id'])
+
+        @logger.info "Found: #{dash['title']} - #{url}"
+        found.each do |graph|
+          @logger.debug "  #{graph[:title]}"
+          graph[:queries].each do |query|
+            @logger.debug "    #{query}"
+          end
+        end
+
+        @logger.debug ""
       end
     end
+  end
+
+  def print_screenboard_matches(metric)
+    regex = metric_regex(metric)
+    @logger.info("Looking for screenboards using metric: #{regex.inspect}")
+    _, screens = @dog_client.get_all_screenboards()
+
+    screens['screenboards'].each do |screen|
+      status_code, definition = @dog_client.get_screenboard(screen['id'])
+
+      if status_code != '200'
+        @logger.warn("Got status code #{status_code} querying dashboard #{dash['id']}")
+        next
+      end
+
+      found = find_screenboard_graphs(definition, regex)
+
+      if found.length > 0
+        url = template('screenboard', id: screen['id'])
+
+        @logger.info "Found: #{screen['title']} - #{url}"
+        found.each do |graph|
+          @logger.debug "  #{graph[:title]}"
+          graph[:queries].each do |query|
+            @logger.debug "    #{query}"
+          end
+        end
+
+        @logger.debug ""
+      end
+    end
+  end
+
+  def run()
+    metric = @args[0] || raise("You must specify a metric name!")
+
+    print_dashboard_matches(metric)
+    print_screenboard_matches(metric)
   end
 end
 
