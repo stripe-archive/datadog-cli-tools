f3a1dd452fd63bd572f0a73997ae1305b6b0d18b
Kris Reeves <krisreeves@stripe.com>
2017-02-01 09:45:26 -0800

    Convert monitor search, update readme

--- monitors-using-metrics.rb
+++ monitors-using-metrics.rb
@@ -3,18 +3,40 @@
 require_relative 'command'
 
 class MonitorsUsingMetrics < Command
+  def parse_args(options, parser)
+    options[:is_regex] = false
+
+    parser.on('-r', '--[no-]regex', 'Input is a regular expression (don\'t quote)') do |v|
+      options[:is_regex] = v
+    end
+  end
+
+  def metric_regex(metric)
+    if @options[:is_regex]
+      /#{metric}/
+    else
+      /#{Regexp.quote(metric)}/
+    end
+  end
+
+  def print_monitor_matches(metric)
+    regex = metric_regex(metric)
+    @logger.info("Looking for monitors using metric: #{regex.inspect}")
+    _, monitors = @dog_client.get_all_monitors()
+
+    monitors.each do |monitor|
+      next unless monitor['query'] =~ regex
+      url = template('monitor', id: monitor['id'])
+
+      @logger.info "Found: #{monitor['name']} - #{url}"
+      @logger.debug "  #{monitor['query']}"
+    end
+  end
 
   def run()
-    metric = ARGV[0] || raise("You must specify a metric name!")
-    @logger.info("Looking for monitors using metric: '#{metric}'")
-    result = @dog_client.get_all_monitors()
+    metric = @args[0] || raise("You must specify a metric name!")
 
-    monitors = result[1]
-    monitors.each do |monitor|
-      if monitor['query'] =~ /#{Regexp.quote(metric)}/
-        puts("#{monitor['id']}")
-      end
-    end
+    print_monitor_matches(metric)
   end
 end
 
